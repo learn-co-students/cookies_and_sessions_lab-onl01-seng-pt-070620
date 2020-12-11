d28cb2765a433213be205bbbd5fb79caf9fb15de
Maddog164 <denise@henderson3.us>
2020-12-11 11:45:56 -0500

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,13 @@
 class ProductsController < ApplicationController
 
+    def index
+        @cart = cart
+    end
+
+    def add
+        @item = item.findparams[:id]
+        current_cart << params[:product]
+        redirect_to root_path
+    end
+
 end
